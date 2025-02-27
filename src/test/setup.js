b9a64efd18d0d4c7b37447608a9746ecf929a9e3
Carmani H. Jackson <charrisjackson422@gmail.com>
2025-02-27 15:24:45 -0500

    testing done

--- src/test/setup.js
+++ src/test/setup.js
@@ -9,18 +9,3 @@ afterEach(() => {
   cleanup();
 });
 
-// If you have any global mocks or other setup, add them here
-// For example:
-// global.fetch = vi.fn();
-
-// You can also add custom matchers here
-// For example:
-// expect.extend({
-//   toBeInRange(received, floor, ceiling) {
-//     const pass = received >= floor && received <= ceiling;
-//     return {
-//       pass,
-//       message: () => `expected ${received} ${pass ? 'not ' : ''}to be in range [${floor}, ${ceiling}]`,
-//     };
-//   },
-// });
